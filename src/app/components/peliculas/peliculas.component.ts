f6dbbc38038b497becac5f441f21357b15caf7e1
Juan Fermin <juanfob.dev@gmail.com>
2021-01-05 13:37:41 +0100

    clase 108 min 8:24

--- src/app/components/peliculas/peliculas.component.ts
+++ src/app/components/peliculas/peliculas.component.ts
@@ -7,10 +7,15 @@ import { Component, DoCheck, OnDestroy, OnInit } from '@angular/core';
 })
 export class PeliculasComponent implements OnInit, DoCheck, OnDestroy {
   public titulo;
+  public peliculas: Array<any>;
 
   constructor() {
-    console.log('constructor lanzado');
     this.titulo = 'componente peliculas';
+    this.peliculas = [
+      {title: "Spiderman 4", image: "https://cnet1.cbsistatic.com/img/rfzZ-7G32v_qEt2uCD0b4KB2rho=/940x0/2019/03/26/13d0a566-7355-4381-be24-dee281227504/spider-man-far-from-home-promo-image-1.jpg"},
+      {title: "Vengadores Endgame", image: "https://as.com/tikitakas/imagenes/2019/04/26/portada/1556258369_131914_1556258703_noticia_normal.jpg"},
+      {title: "Batman vs Superman", image: "https://i.pinimg.com/originals/08/cb/c3/08cbc3566f6ed36c05811c668113ad6e.jpg"}
+    ]
    }
 
   ngOnInit(): void {
